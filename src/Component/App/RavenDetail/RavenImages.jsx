bc71640881ebd6e21399878705677440b0b10f3c
Ridhim-Immanent <ridhim.immanent@gmail.com>
2024-06-28 17:11:35 +0530

    Css update

--- src/Component/App/RavenDetail/RavenImages.jsx
+++ src/Component/App/RavenDetail/RavenImages.jsx
@@ -15,7 +15,7 @@ const RavenImages = ({images}) => {
         <div>
             <div className='grid grid-cols-4 items-start justify-start mx-[-10px] xsm:flex-wrap' style={{ rowGap: '20px' }}>
                 { images?.length ? images.map((img, index) => (
-                    <div key={index} className="w-1/2  px-[10px]">
+                    <div key={index} className="  px-[10px]">
                         <div className="relative overflow-hidden rounded-[20px] group cursor-pointer" onClick={() => handleImageClick(index)}>
                             <Image src={img} alt={`raven image ${index + 1}`} width="100" height="100" className="!h-[100%] !w-[100%] max-h-[200px] object-fill relative z-0" />
                             <div className="h-[100%] w-[100%] absolute left-0 top-0 bg-[#00000080] flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
